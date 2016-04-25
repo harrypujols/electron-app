feb18798a4fef6828acd480a223c99cbb86f0b3c
Harry Pujols <harry.pujols@pgmmb.com>
2016-04-25 12:37:25 -0400

    switching to gulp electron

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,32 @@
 var gulp = require('gulp');
-var exec = require('gulp-exec');
+var electron = require('gulp-electron');
+var packageJson = require('./src/package.json');
 
-gulp.task('exec', function() {
-  var options = {
-    continueOnError: false,
-    pipeStdout: false
-  };
-  var reportOptions = {
-  	err: true,
-  	stderr: true,
-  	stdout: true
-  }
-  gulp.src('./**/**')
-    .pipe(exec('electron-packager ./src --platform=darwin --arch=x64', options))
-    .pipe(exec.reporter(reportOptions));
+gulp.task('electron', function() {
+
+    gulp.src('')
+    .pipe(electron({
+        src: './app',
+        packageJson: packageJson,
+        release: './release',
+        cache: './cache',
+        version: 'v0.37.4',
+        packaging: true,
+        platforms: ['win32-ia32', 'darwin-x64'],
+        platformResources: {
+            darwin: {
+                CFBundleDisplayName: packageJson.name,
+                CFBundleIdentifier: packageJson.name,
+                CFBundleName: packageJson.name,
+                CFBundleVersion: packageJson.version,
+                icon: './icons/icon.icns'
+            },
+            win: {
+                "version-string": packageJson.version,
+                "file-version": packageJson.version,
+                "product-version": packageJson.version
+            }
+        }
+    }))
+    .pipe(gulp.dest(''));
 });
